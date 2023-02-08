0fb0b1eec50b14ad7328586435bc6c50268f915d
fika4life <fistewie@gmail.com>
2023-02-08 22:12:27 +0100

    replaces id field with slug

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -1,9 +1,22 @@
-'use strict';
+"use strict";
 
 /**
- * post controller
+ * pst controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController("api::post.post", ({ strapi }) => ({
+  // Query by slug
+  async findOne(ctx) {
+    // thanks to the custom route we have now a slug variable
+    // instead of the default id
+    const { slug } = ctx.params;
+    const entity = await strapi.db.query("api::post.post").findOne({
+      where: { slug },
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
